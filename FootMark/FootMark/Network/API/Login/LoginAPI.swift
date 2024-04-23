133147970c09ec9415e0144b033e4a5c42c10d7b
ParkSY0919 <qhr498@gmail.com>
2024-04-23 23:47:26 +0900

    [Feat] #4 - Add Google SocialLogin Feature

---
+++ FootMark/FootMark/Network/API/Login/LoginAPI.swift
@@ -0,0 +1,136 @@
+//
+//  LoginAPI.swift
+//  FootMark
+//
+//  Created by 박신영 on 4/16/24.
+//
+
+import Alamofire
+import UIKit
+import KeychainSwift
+
+final class LoginAPI : BaseAPI {
+    static let shared = LoginAPI()
+    var loginVC = LoginViewController()
+    let keychain = KeychainSwift()
+    private override init() {}
+}
+
+extension LoginAPI {
+   
+   // MARK: - 소셜로그인
+   
+   public func SocialSignin(authCode: String?, provider: String) {
+      
+   }
+   
+    var baseURL: String { return Config.baseURL }
+    // 로그인 API
+    
+   public func getAccessToken(authCode: String?, provider: String) {
+         let headers: HTTPHeaders = [
+            "Accept": "application/json"
+         ]
+         // Body 설정
+         let parameters: Parameters = [
+            "authCode" : authCode!,
+         ]
+         AF.request("\(baseURL)/oauth/\(provider)/token", method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).validate(statusCode: 200..<300)
+            .responseDecodable(of: LoginDTO.self) { response in
+               switch response.result {
+               case .success(let loginDTO):
+                  print("\(provider) 로그인 성공")
+                  print("AccessToken: \(loginDTO.data.accessToken)")
+                  print("refreshToken: \(loginDTO.data.refreshToken)")
+                  let keychain = KeychainSwift()
+                  keychain.set(loginDTO.data.accessToken, forKey: "accessToken")
+                  keychain.set(loginDTO.data.refreshToken, forKey: "refreshToken")
+                  
+                case .failure(_):
+                    if let statusCode = response.response?.statusCode {
+                        switch statusCode {
+                        case 400:  // 기존 회원
+                            print("로그인 실패")
+//                            DispatchQueue.main.async {
+//                                let alertController = UIAlertController(title: "로그인 실패", message: "해당 이메일로 이미 가입한 계정이 있습니다.\n다른 소셜 플랫폼에서 가입하였다면, 해당 플랫폼을 통해 로그인을 시도해주십시오.", preferredStyle: .alert)
+//                                let action = UIAlertAction(title: "확인", style: .default)
+//                                alertController.addAction(action)
+//                                
+//                                // 현재 활성화된 뷰 컨트롤러를 찾아서 알림창을 띄웁니다.
+//                                if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+//                                   let delegate = windowScene.delegate as? SceneDelegate,
+//                                   let window = delegate.window,
+//                                   let viewController = window.rootViewController {
+//                                    viewController.present(alertController, animated: true, completion: nil)
+//                                }
+//                            }
+                        case 401:  // 인증 실패
+                            print("로그인 실패: 인증 실패")
+                        case 404:
+                            print("로그인 실패: Email Not Found")
+//                            DispatchQueue.main.async {
+//                                let alertController = UIAlertController(title: "로그인 실패", message: "Github 이메일이 존재하지 않습니다.\n다른 플랫폼을 이용한 로그인 혹은 Github 이메일을 등록 후 시도해주세요", preferredStyle: .alert)
+//                                let action = UIAlertAction(title: "확인", style: .default)
+//                                alertController.addAction(action)
+//                                
+//                                // 현재 활성화된 뷰 컨트롤러를 찾아서 알림창을 띄웁니다.
+//                                if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+//                                   let delegate = windowScene.delegate as? SceneDelegate,
+//                                   let window = delegate.window,
+//                                   let viewController = window.rootViewController {
+//                                    viewController.present(alertController, animated: true, completion: nil)
+//                                }
+//                            }
+                            
+                        case 500...599:  // 서버 에러
+                            print("로그인 실패: 서버 에러")
+                        default:
+                            print("로그인 실패: 알 수 없는 에러")
+                        }
+                    }
+                    
+                }
+            }
+    }
+   
+   
+    public func refreshAccessToken(completion: @escaping (Result<LoginDTO, Error>) -> Void){
+        let keychain = KeychainSwift()
+        guard let refreshToken = keychain.get("refreshToken") else {
+            print("No refreshToken found in keychain.")
+            return
+        }
+        let headers: HTTPHeaders = [
+            "Accept": "application/json"
+        ]
+        // Body 설정
+        let parameters: Parameters = [
+            "refreshToken" : refreshToken,
+        ]
+        AF.request("\(baseURL)/api/token/access", method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).validate(statusCode: 200..<300)
+            .responseDecodable(of: LoginDTO.self) { response in
+                switch response.result {
+                case .success(let loginDTO):
+                    print("재발급 로그인 성공")
+                    print("AccessToken: \(loginDTO.data.accessToken)")
+                    print("refreshToken: \(loginDTO.data.refreshToken)")
+                    let keychain = KeychainSwift()
+                    keychain.set(loginDTO.data.accessToken, forKey: "accessToken")
+                    keychain.set(loginDTO.data.refreshToken, forKey: "refreshToken")
+                    completion(.success(loginDTO))
+                case .failure(let error):
+                    if let statusCode = response.response?.statusCode {
+                        switch statusCode {
+                        case 400...499:  // 클라이언트 에러
+                            print("로그인 실패: 클라이언트 에러")
+                        case 500...599:  // 서버 에러
+                            print("로그인 실패: 서버 에러")
+                        default:
+                            print("로그인 실패: 알 수 없는 에러")
+                        }
+                    }
+                    completion(.failure(error))
+                }
+            }
+    }
+}
--- FootMark/FootMark/Presentation/Sign/View/LoginView.swift
+++ FootMark/FootMark/Presentation/Sign/View/LoginView.swift
@@ -10,37 +10,114 @@ import SnapKit
 import Then
 
 class LoginView: BaseView {
-    
-    // MARK: - Properties
-    
-   let idTextField: UITextField = {
-        let textField = UITextField()
-        textField.placeholder = "ID"
-        textField.borderStyle = .roundedRect
-        return textField
-    }()
-    
-    private let passwordTextField: UITextField = {
-        let textField = UITextField()
-        textField.placeholder = "Password"
-        textField.borderStyle = .roundedRect
-        textField.isSecureTextEntry = true
-        return textField
-    }()
-    
-    // MARK: - Private Methods
-    
+   
+   // MARK: - Properties
+   
+   // Logo & Label
+   let footMarkLogo = UIImageView(image: Image.footMarkLogo)
+   let footMarkLabel = UILabel().then{
+      $0.setVigaFont(text: "FootMark", size: 25.0, weight: .black, letterSpacing: 1.37)
+      $0.textAlignment = .center
+   }
+   
+   //apple SignIn Button
+   let appleSignInButton = UIButton()
+   let appleLogo = UIImageView(image:Image.apple_SignInButton)
+   let appleSignInTitle = UILabel().then{
+      $0.setPretendardFont(text: "Sign in with Apple", size: 17.0, weight: .regular, letterSpacing: 1.23)
+      $0.textAlignment = .center
+      $0.textColor = UIColor.White()
+   }
+   //Google SignIn Button
+   let googleSignInButton = UIButton()
+   let googleLogo = UIImageView(image:Image.google_SignInButton)
+   let googleLogoBackGround = UIView()
+   let googleSignInTitle = UILabel().then{
+      $0.setPretendardFont(text: "Sign in with Google", size: 17.0, weight: .regular, letterSpacing: 1.23)
+      $0.textAlignment = .center
+      $0.textColor = UIColor.White()
+   }
+   
+   override func setUI() {
+      addSubviews(footMarkLogo, footMarkLabel)
+      addSubviews(googleSignInButton, appleSignInButton)
+      
+      //AppleSignInButton
+      appleSignInButton.addSubviews(appleLogo, appleSignInTitle)
+      
+      
+      //GoogleSignInButton
+      googleSignInButton.addSubviews(googleLogoBackGround, googleSignInTitle)
+      googleLogoBackGround.addSubview(googleLogo)
+      
+      
+   }
+   
+   
+   
    override func setLayout() {
-        addSubviews(idTextField, passwordTextField)
-
-        
-        idTextField.snp.makeConstraints {
-            $0.top.leading.trailing.equalToSuperview()
-        }
-        
-        passwordTextField.snp.makeConstraints {
-            $0.top.equalTo(idTextField.snp.bottom).offset(16)
-            $0.leading.trailing.equalToSuperview()
-        }
-    }
+      //Logo Design
+      footMarkLogo.snp.makeConstraints{
+         $0.centerX.equalTo(self.safeAreaLayoutGuide.snp.centerX)
+         $0.centerY.equalTo(self.safeAreaLayoutGuide.snp.centerY).offset(-82)
+         $0.width.equalTo(192)
+         $0.height.equalTo(177)
+      }
+      
+      footMarkLabel.snp.makeConstraints {
+         $0.centerX.equalTo(self.safeAreaLayoutGuide.snp.centerX)
+         $0.bottom.equalTo(footMarkLogo.snp.top).offset(-20)  // logo 위에 위치
+         $0.leading.equalTo(self.safeAreaLayoutGuide.snp.leading).offset(20)
+         $0.trailing.equalTo(self.safeAreaLayoutGuide.snp.trailing).offset(-20)
+      }
+      
+      appleLogo.snp.makeConstraints {
+         $0.centerY.equalToSuperview()
+         $0.leading.equalToSuperview().offset(24)
+         $0.width.equalTo(17)
+         $0.height.equalTo(20)
+      }
+      
+      appleSignInTitle.snp.makeConstraints {
+         $0.centerY.equalToSuperview()
+         $0.centerX.equalToSuperview()
+      }
+      appleSignInButton.snp.makeConstraints {
+         $0.centerX.equalTo(self.safeAreaLayoutGuide.snp.centerX)
+         $0.top.equalTo(footMarkLogo.snp.bottom).offset(42)
+         $0.leading.equalTo(self.safeAreaLayoutGuide.snp.leading).offset(20)
+         $0.trailing.equalTo(self.safeAreaLayoutGuide.snp.trailing).offset(-20)
+         $0.height.equalTo(48)
+      }
+      
+      appleSignInButton.backgroundColor = UIColor.black
+      appleSignInButton.layer.cornerRadius = 12
+      googleLogo.snp.makeConstraints {
+         $0.width.height.equalTo(17)
+         $0.centerX.centerY.equalToSuperview()
+      }
+      googleLogoBackGround.snp.makeConstraints{
+         $0.centerY.equalToSuperview()
+         $0.leading.equalToSuperview().offset(24)
+         $0.width.height.equalTo(20)
+      }
+      googleLogoBackGround.backgroundColor = .white
+      googleLogoBackGround.layer.cornerRadius = 10
+      googleLogoBackGround.clipsToBounds = true
+      
+      googleSignInTitle.snp.makeConstraints {
+         $0.centerY.equalToSuperview()
+         $0.centerX.equalToSuperview()
+      }
+      googleSignInButton.snp.makeConstraints {
+         $0.centerX.equalTo(self.safeAreaLayoutGuide.snp.centerX)
+         $0.top.equalTo(appleSignInButton.snp.bottom).offset(20)
+         $0.leading.equalTo(self.safeAreaLayoutGuide.snp.leading).offset(20)
+         $0.trailing.equalTo(self.safeAreaLayoutGuide.snp.trailing).offset(-20)
+         $0.height.equalTo(48)
+      }
+      googleSignInButton.backgroundColor = UIColor(red: 0.25, green: 0.52, blue: 0.95, alpha: 1.0)
+      googleSignInButton.layer.cornerRadius = 12
+      
+   }
 }
