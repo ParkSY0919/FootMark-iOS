b165ebded1f2cc600d6cdd8f992d4b9c5d408ff5
ParkSY0919 <qhr498@gmail.com>
2024-04-24 22:01:47 +0900

    [Feat] #4 - Add Apple SocialLogin Feature

--- FootMark/FootMark/Presentation/Sign/ViewController/LoginViewController.swift
+++ FootMark/FootMark/Presentation/Sign/ViewController/LoginViewController.swift
@@ -15,24 +15,27 @@ import GoogleSignIn
 
 class LoginViewController: BaseViewController, ASAuthorizationControllerDelegate {
    
-   // MARK: - Properties
+    // MARK: - Properties
    
    let keychain = KeychainSwift()
    var loginView = LoginView()
    
    override func loadView() {
-      view = loginView
+       view = loginView
    }
-   
-   // MARK: - View Life Cycle
-   
-   override func viewDidLoad() {
-      super.viewDidLoad()
-      print("이미 로드됨")
-   }
+    
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       print("이미 로드됨")
+    }
    
    
    override func setAddTarget() {
+      // Apple 로그인 버튼
+      loginView.appleSignInButton.addTarget(self, action: #selector(handleAppleSignIn), for: .touchUpInside)
+      
       // Google 로그인 버튼
       loginView.googleSignInButton.addTarget(self, action: #selector(handleGoogleSignIn), for: .touchUpInside)
    }
@@ -56,5 +59,55 @@ class LoginViewController: BaseViewController, ASAuthorizationControllerDelegate
       }
    }
    
+   @objc private func handleAppleSignIn() {
+       let appleIDProvider = ASAuthorizationAppleIDProvider()
+       let request = appleIDProvider.createRequest()
+       request.requestedScopes = [.fullName, .email]
+       let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+       authorizationController.delegate = self
+       authorizationController.presentationContextProvider = self
+       authorizationController.performRequests()
+   }
+  
+   func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+       if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+           guard let appleIDToken = appleIDCredential.identityToken else {
+               print("Unable to fetch identity token")
+               return
+           }
+           guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+               print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+               return
+           }
+           switch authorization.credential {
+           case let appleIDCredential as ASAuthorizationAppleIDCredential:
+               if  let authorizationCode = appleIDCredential.authorizationCode,
+                   let identityToken = appleIDCredential.identityToken,
+                   let authCodeString = String(data: authorizationCode, encoding: .utf8),
+                   let identifyTokenString = String(data: identityToken, encoding: .utf8) {
+                   print("authCodeString: \(authCodeString)")
+                   print("identifyTokenString: \(identifyTokenString)")
+                   LoginAPI.shared.getAccessToken(authCode: identifyTokenString, provider: "apple")
+               }
+           default:
+               break
+           }
+       }
+   }
+   
+   func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+       // Handle error.
+       print("Sign in with Apple errored: \(error)")
+   }
+   
 }
+
+extension LoginViewController: ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        // Apple 로그인 인증 창 띄우기
+        return self.view.window ?? UIWindow()
+    }
+}
+
+
 
