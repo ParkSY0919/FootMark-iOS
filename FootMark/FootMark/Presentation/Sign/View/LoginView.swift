10bd3f75cb0e6ba31fe9af2eca797f921263ae04
ParkSY0919 <qhr498@gmail.com>
2024-05-31 13:34:19 +0900

    [Feat] #4 - Moya 사용하여 SocialLogin 구현 및 refreshToken 자동 재발급 구현

--- FootMark/FootMark/Presentation/Sign/View/LoginView.swift
+++ FootMark/FootMark/Presentation/Sign/View/LoginView.swift
@@ -12,11 +12,12 @@ import Then
 class LoginView: BaseView {
    
    // MARK: - Properties
-   
+
    // Logo & Label
    let footMarkLogo = UIImageView(image: Image.footMarkLogo)
    let footMarkLabel = UILabel().then{
-      $0.setVigaFont(text: "FootMark", size: 25.0, weight: .black, letterSpacing: 1.37)
+      $0.setVigaFont(text: "FootMark", size: 25.0, weight: .bold, letterSpacing: 1.37)
+      $0.textColor = .primary
       $0.textAlignment = .center
    }
    
@@ -28,6 +29,7 @@ class LoginView: BaseView {
       $0.textAlignment = .center
       $0.textColor = UIColor.White()
    }
+   
    //Google SignIn Button
    let googleSignInButton = UIButton()
    let googleLogo = UIImageView(image:Image.google_SignInButton)
@@ -44,54 +46,47 @@ class LoginView: BaseView {
       
       //AppleSignInButton
       appleSignInButton.addSubviews(appleLogo, appleSignInTitle)
-      
+      appleSignInButton.do {
+         $0.backgroundColor = .black
+         $0.layer.cornerRadius = 12
+      }
       
       //GoogleSignInButton
       googleSignInButton.addSubviews(googleLogoBackGround, googleSignInTitle)
       googleLogoBackGround.addSubview(googleLogo)
-      
-      
    }
-   
-   
    
    override func setLayout() {
       //Logo Design
       footMarkLogo.snp.makeConstraints{
          $0.centerX.equalTo(self.safeAreaLayoutGuide.snp.centerX)
-         $0.centerY.equalTo(self.safeAreaLayoutGuide.snp.centerY).offset(-82)
+         $0.centerY.equalToSuperview().offset(-100)
          $0.width.equalTo(192)
          $0.height.equalTo(177)
       }
       
       footMarkLabel.snp.makeConstraints {
          $0.centerX.equalTo(self.safeAreaLayoutGuide.snp.centerX)
-         $0.bottom.equalTo(footMarkLogo.snp.top).offset(-20)  // logo 위에 위치
-         $0.leading.equalTo(self.safeAreaLayoutGuide.snp.leading).offset(20)
-         $0.trailing.equalTo(self.safeAreaLayoutGuide.snp.trailing).offset(-20)
+         $0.top.equalTo(footMarkLogo.snp.bottom).offset(20)
       }
       
+      appleSignInButton.snp.makeConstraints {
+         $0.top.equalTo(footMarkLabel.snp.bottom).offset(40)
+         $0.leading.equalTo(self.safeAreaLayoutGuide.snp.leading).offset(20)
+         $0.trailing.equalTo(self.safeAreaLayoutGuide.snp.trailing).offset(-20)
+         $0.height.equalTo(48)
+      }
       appleLogo.snp.makeConstraints {
          $0.centerY.equalToSuperview()
          $0.leading.equalToSuperview().offset(24)
          $0.width.equalTo(17)
          $0.height.equalTo(20)
       }
-      
       appleSignInTitle.snp.makeConstraints {
          $0.centerY.equalToSuperview()
          $0.centerX.equalToSuperview()
       }
-      appleSignInButton.snp.makeConstraints {
-         $0.centerX.equalTo(self.safeAreaLayoutGuide.snp.centerX)
-         $0.top.equalTo(footMarkLogo.snp.bottom).offset(42)
-         $0.leading.equalTo(self.safeAreaLayoutGuide.snp.leading).offset(20)
-         $0.trailing.equalTo(self.safeAreaLayoutGuide.snp.trailing).offset(-20)
-         $0.height.equalTo(48)
-      }
       
-      appleSignInButton.backgroundColor = UIColor.black
-      appleSignInButton.layer.cornerRadius = 12
       googleLogo.snp.makeConstraints {
          $0.width.height.equalTo(17)
          $0.centerX.centerY.equalToSuperview()
@@ -110,12 +105,10 @@ class LoginView: BaseView {
          $0.centerX.equalToSuperview()
       }
       googleSignInButton.snp.makeConstraints {
-         $0.centerX.equalTo(self.safeAreaLayoutGuide.snp.centerX)
          $0.top.equalTo(appleSignInButton.snp.bottom).offset(20)
-         $0.leading.equalTo(self.safeAreaLayoutGuide.snp.leading).offset(20)
-         $0.trailing.equalTo(self.safeAreaLayoutGuide.snp.trailing).offset(-20)
-         $0.height.equalTo(48)
+         $0.leading.trailing.height.equalTo(appleSignInButton)
       }
+      
       googleSignInButton.backgroundColor = UIColor(red: 0.25, green: 0.52, blue: 0.95, alpha: 1.0)
       googleSignInButton.layer.cornerRadius = 12
       
