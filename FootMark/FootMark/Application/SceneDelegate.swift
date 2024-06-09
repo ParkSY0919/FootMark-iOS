13f718ba923f6b44bed1ecc036bb77c32ce17b5b
Yun, Seongeun <0420yun@gamil.com>
2024-06-09 23:26:26 +0900

    [Fix] #25 - SceneDelegate 변경

--- FootMark/FootMark/Application/SceneDelegate.swift
+++ FootMark/FootMark/Application/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
       
       self.window = UIWindow(windowScene: windowScene)
       
-      let navigationController = UINavigationController(rootViewController: DiaryViewController())
+      let navigationController = UINavigationController(rootViewController: LoginViewController())
       
       self.window?.rootViewController = navigationController
       
